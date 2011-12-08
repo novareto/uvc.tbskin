7861e1a8e4bdc7219d1aeb1d8280f3e86ce3df05
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-12-08 08:55:55 +0000

    Changed the name of the folder from nva to uvc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 version = '1.0'
 
-setup(name='nva.tbskin',
+setup(name='uvc.tbskin',
       version=version,
       description="",
       long_description=open("README.txt").read() + "\n" +
@@ -18,7 +18,7 @@ setup(name='nva.tbskin',
       url='http://svn.plone.org/svn/collective/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['nva'],
+      namespace_packages=['uvc'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
@@ -28,7 +28,7 @@ setup(name='nva.tbskin',
       ],
       entry_points={
          'fanstatic.libraries': [
-            'nva.tbskin = nva.tbskin.app:library',
+            'uvc.tbskin = uvc.tbskin.app:library',
             ],
          'z3c.autoinclude.plugin': 'target=uvcsite', 
       }
