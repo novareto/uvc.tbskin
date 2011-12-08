fd9625b81e393da5e879467b86112b330ea9a341
trollfot <trollfot@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-12-08 10:56:42 +0000

    Added TB elements

--- uvc/tbskin/skin.py
+++ uvc/tbskin/skin.py
@@ -3,14 +3,30 @@
 # ck@novareto.de
 
 import grok
-
-from zope.publisher.interfaces import browser
+from megrok import layout
 from uvc.layout.layout import IUVCSkin
+from zope.interface import Interface
+from zope.publisher.interfaces import browser
+from zope.container.interfaces import IContainer
+from zope.traversing.browser import absoluteURL
+
+grok.templatedir('templates')
 
 
 class ITBSkinLayer(grok.IDefaultBrowserLayer):
     pass
 
 
-class ITBSkin(IUVCSkin):
+class ITBSkin(ITBSkinLayer, IUVCSkin):
     grok.skin('tbskin')
+
+
+class Layout(layout.Layout):
+    grok.context(Interface)
+    grok.layer(ITBSkinLayer)
+    grok.name('uvc.layout')
+
+    def update(self):
+        self.base = absoluteURL(self.context, self.request)
+        if IContainer.providedBy(self.context) and self.base[:-1] != '/':
+            self.base = self.base + '/'
