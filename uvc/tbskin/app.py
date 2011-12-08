6913408a631d39653aa64297b64265a5f5c3c44a
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-12-08 09:38:28 +0000

    changes

--- uvc/tbskin/app.py
+++ uvc/tbskin/app.py
@@ -18,3 +18,74 @@ class TBSkinViewlet(resourceviewlet.ResourceViewlet):
 
     def update(self):
         [x.need() for x in self.resources]
+
+
+
+#
+### Examples
+#
+
+import uvcsite
+from zope import interface, schema
+
+grok.templatedir('templates')
+
+
+class Index(uvcsite.Page):
+    grok.context(interface.Interface)
+
+class HTMLTags(uvcsite.Page):
+    grok.context(interface.Interface)
+
+
+class AppsExample(uvcsite.SubMenu):
+    grok.title('Applications')
+    grok.context(interface.Interface)
+    grok.viewletmanager(uvcsite.IGlobalMenu)
+
+    action = "lars"
+
+class App1(uvcsite.MenuItem):
+    grok.title('Application One')
+    grok.viewletmanager(AppsExample)
+
+    action = u"myform"
+
+class IPerson(interface.Interface):
+
+    id = schema.TextLine(
+       title = u"Id",
+       description = u"Not more then 3 letters",
+       max_length=3,
+       )
+
+    name = schema.TextLine(
+       title = u"Name",
+       description = u"Please give your first name Here",
+       )
+
+class MyForm(uvcsite.Form):
+    grok.context(interface.Interface)
+    fields = uvcsite.Fields(IPerson)
+
+    label = u"LABEL"
+    description = u"DEscription"
+
+    @uvcsite.action('Save')
+    def handle_save(self):
+        data, errors = self.extractData()
+        if errors:
+            self.flash(u'Damn a Error')
+            return
+        self.flash(u'Success')
+
+
+class LoggedinAs(uvcsite.MenuItem):
+    grok.context(interface.Interface)
+    grok.viewletmanager(uvcsite.IPersonalPreferences)
+    
+    @property
+    def title(self):
+        return "You are logged in as MASTER"
+
+
