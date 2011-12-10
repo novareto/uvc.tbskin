e77f9d09e80cf0a2186586e0c155c7a1a56df6cf
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-12-10 17:30:26 +0000

    remove app_skin.py

--- uvc/tbskin/skin.py
+++ uvc/tbskin/skin.py
@@ -3,12 +3,19 @@
 # ck@novareto.de
 
 import grok
+import zope.security.management
+
+
+from grokcore.traverser import Traverser
 from megrok import layout
 from uvc.layout.layout import IUVCSkin
+from zope.component.hooks import setSite, getSite
+from zope.container.interfaces import IContainer
 from zope.interface import Interface
 from zope.publisher.interfaces import browser
-from zope.container.interfaces import IContainer
+from zope.publisher.skinnable import setDefaultSkin
 from zope.traversing.browser import absoluteURL
+
 
 grok.templatedir('templates')
 
@@ -30,4 +37,21 @@ class Layout(layout.Layout):
         self.base = absoluteURL(self.context, self.request)
         if IContainer.providedBy(self.context) and self.base[:-1] != '/':
             self.base = self.base + '/'
+
+
+### Needed For Baseregisty
+#@grok.subscribe(grok.IApplication, grok.IBeforeTraverseEvent)
+#def setSkin(app, event=None):
+#    if getSite() != app:
+#        setSite(app)
+#    request = zope.security.management.getInteraction().participations[0]
+#    setDefaultSkin(request)
+#
+#
+#class appTraverser(Traverser):
+#    grok.context(grok.IApplication)
+#
+#    def traverse(self, name):
+#        setSkin(self.context)
+#        return self.context.get(name)
 
